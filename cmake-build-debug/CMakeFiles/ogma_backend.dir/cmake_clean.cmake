95dc3734ea3fe30c386672166d15bc707cb5e6f1
Tim Kuzhagaliyev <tim.kuzh@gmail.com>
2019-07-14 18:20:30 +0100

    Create project skeleton

---
+++ cmake-build-debug/CMakeFiles/ogma_backend.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ogma_backend.dir/main.cpp.o"
+  "ogma_backend.pdb"
+  "ogma_backend"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ogma_backend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
